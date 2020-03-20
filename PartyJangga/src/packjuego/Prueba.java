6fa69c45f89b4aa701943d1876c104bce7c70773
AvalonRhythm <hugoroblesmourino@gmail.com>
2020-03-20 18:59:06 +0100

    coment

--- PartyJangga/src/packjuego/Prueba.java
+++ PartyJangga/src/packjuego/Prueba.java
@@ -12,5 +12,5 @@ public class Prueba {
 		System.out.println("Realiza la siguiente prueba! (si no la cumples no pasa nada, pero no seas soso)");
 		System.out.println(this.descripcion);
 	}
-	//kk
+	//hola k ase
 }
