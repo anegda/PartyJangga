385d2724f5a9d1b1a0df1f66b3c7128184d1aa13
Ane <Ane@192.168.1.109>
2020-03-23 23:38:50 +0100

    porfi que vaya

--- PartyJangga/src/packjuego/CasillaRetroceder.java
+++ PartyJangga/src/packjuego/CasillaRetroceder.java
@@ -9,14 +9,12 @@ public class CasillaRetroceder extends Casilla{
 	private int cantRetroceder;
 	
 	//constructora
-	
 	public CasillaRetroceder(int idCasilla, int pRetroceder){
 		super(idCasilla);
 		cantRetroceder = pRetroceder;
 	}
 	
 	//metodos adicionales
-	
 	public void usar(Jugador pJugador) {
 		pJugador.retrocederCasilla(cantRetroceder);
 	}
